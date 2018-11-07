6c4c332b0defea05355e5bc002e45c44d0eea872
mfgomezb <mgomezb89@gmail.com>
2018-11-07 15:03:26 +0100

    modified folder structure and added .gitignore

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const servers = {
-  primary: "exercise5_mongodb_1:27017",
-  replica: "exercise5_replica_1:27018"
-  // primary: "127.0.0.1:27017",
-  // replica: "127.0.0.1:27018"
+  // primary: "exercise5_mongodb_1:27017",
+  // replica: "exercise5_replica_1:27018"
+  primary: "127.0.0.1:27017",
+  replica: "127.0.0.1:27018"
 };
 const database = "cabify_bootcamp";
 
